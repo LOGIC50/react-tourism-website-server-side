a1ba10efe8581772b65731896db5b979e8eb8845
LOGIC50 <ahmedjafrulislam@gmail.com>
2021-12-27 10:32:06 +0600

    final submit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require('express');
-const { MongoClient } = require('mongodb');
-require('dotenv').config();
-const cors = require('cors');
+const express = require("express");
+const { MongoClient } = require("mongodb");
+require("dotenv").config();
+const cors = require("cors");
+var ObjectId = require("mongodb").ObjectId;
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,74 +12,103 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lsk7p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 console.log(uri);
 
-async function run(){
-    try{
-        await client.connect();
-        const database = client.db('tourism');
-        const serviceCollection = database.collection('services');
-        const reviewCollection = database.collection('review');
-        const guideCollection = database.collection('guides');
-        const registerCollection = database.collection('register')
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("tourism");
+    const serviceCollection = database.collection("services");
+    const reviewCollection = database.collection("review");
+    const guideCollection = database.collection("guides");
+    const registerCollection = database.collection("register");
 
-        // POST API
-        app.post('/services', async(req, res) => {
-            const service = req.body;
-            const result = await serviceCollection.insertOne(service);
-            console.log(result);
-            res.json(result);
-        })
+    // POST API
+    app.post("/services", async (req, res) => {
+      const service = req.body;
+      const result = await serviceCollection.insertOne(service);
+      console.log(result);
+      res.json(result);
+    });
 
-        // POST REGISTRATION API
-        app.post('/register/', async(req, res) => {
-            const register = req.body;
-            console.log('hit the post api', register);
-            const result = await registerCollection.insertOne(register); 
-            res.json(result);
-        })
+    // GET SINGLE SERVICE API
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.findOne(query);
+      res.json(result);
+      console.log(result);
+    });
 
-        // GET REGISTRATION API
-        app.get('/register/', async(req, res) => {
-            const cursor = registerCollection.find({});
-            const register = await cursor.toArray();
-            res.json(register);
-        })
+    // POST REGISTRATION API
+    app.post("/register/", async (req, res) => {
+      const register = req.body;
+      console.log("hit the post api", register);
+      const result = await registerCollection.insertOne(register);
+      res.json(result);
+    });
 
-        // GET SERVICE API 
-        app.get('/services', async(req, res) => {
-            const cursor = serviceCollection.find({});
-            const services = await cursor.toArray();
-            res.send(services);
-        })
+    // DELETE SERVICES API
+    app.delete("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      console.log(result);
+      res.json(result);
+    });
 
-        // GET REVIEW API
-        app.get('/review', async(req, res) => {
-            const cursor = reviewCollection.find({});
-            const review = await cursor.toArray();
-            res.send(review);
-        })
+    // DELETE REGISTERED API
+    app.delete("/register/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await registerCollection.deleteOne(query);
+      console.log(result);
+      res.json(result);
+    });
 
-        // GET GUIDE API
-        app.get('/guides', async(req, res) => {
-            const cursor = guideCollection.find({});
-            const guides = await cursor.toArray();
-            res.send(guides);
-        })  
-    }
-    finally{
-        // await client.close();
-    }
+    // GET REGISTRATION API
+    app.get("/register/", async (req, res) => {
+      const cursor = registerCollection.find({});
+      const register = await cursor.toArray();
+      res.json(register);
+    });
+
+    // GET SERVICE API
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find({});
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    // GET REVIEW API
+    app.get("/review", async (req, res) => {
+      const cursor = reviewCollection.find({});
+      const review = await cursor.toArray();
+      res.send(review);
+    });
+
+    // GET GUIDE API
+    app.get("/guides", async (req, res) => {
+      const cursor = guideCollection.find({});
+      const guides = await cursor.toArray();
+      res.send(guides);
+    });
+  } finally {
+    // await client.close();
+  }
 }
 
 run().catch(console.dir);
 
-app.get('/', (req, res) => {
-    res.send('Tourism Server is running');
+app.get("/", (req, res) => {
+  res.send("Tourism Server is running");
 });
 
 app.listen(port, () => {
-    console.log('Server is running', port);
+  console.log("Server is running", port);
 });
